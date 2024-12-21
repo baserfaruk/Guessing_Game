a3178e9157251a791986c69d1661b0206f279848
Faruk Başer <baserfaruk.rs@gmail.com>
2024-12-21 22:37:11 +0300

    v2

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
-use std::io;
+use std::{cmp::Ordering, io};
+use rand::Rng;
 
 fn main() {
 
-    println!("Guess the number!");
-    println!("Please input your guess.");
+    println!("Guess the number! 1-30");
 
-    let mut guess = String::new();
+    let secret_number = rand::rng().random_range(1..=30);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {}",guess);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+         io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 =match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}",guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+
+        }
+    }
+    
 }
